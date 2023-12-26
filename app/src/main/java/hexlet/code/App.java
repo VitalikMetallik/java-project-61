a39779b84a8cb6714b5bc081d407939b77524f59
VitalikMetallik <flash_PS4@mail.ru>
2023-12-26 20:12:27 +0300

    Убрал магические числа из App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,3 +1,4 @@
+
 package hexlet.code;
 
 import hexlet.code.games.Even;
@@ -9,27 +10,34 @@ import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
+    private static final int GAME_CHOICE_GREET = 1;
+    private static final int GAME_CHOICE_EVEN = 2;
+    private static final int GAME_CHOICE_CALC = 3;
+    private static final int GAME_CHOICE_GCD = 4;
+    private static final int GAME_CHOICE_PROGRESSION = 5;
+    private static final int GAME_CHOICE_PRIME = 6;
+    private static final int GAME_CHOICE_EXIT = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Вывод меню выбора игр
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
+        System.out.println(GAME_CHOICE_GREET + " - Greet");
+        System.out.println(GAME_CHOICE_EVEN + " - Even");
+        System.out.println(GAME_CHOICE_CALC + " - Calc");
+        System.out.println(GAME_CHOICE_GCD + " - GCD");
+        System.out.println(GAME_CHOICE_PROGRESSION + " - Progression");
+        System.out.println(GAME_CHOICE_PRIME + " - Prime");
+        System.out.println(GAME_CHOICE_EXIT + " - Exit");
         System.out.println("Your choice: ");
         int gameChoice = scanner.nextInt();
         switch (gameChoice) {
-            case 0 -> scanner.close();
-            case 1 -> Cli.askUsername();
-            case 2 -> Even.runGame();
-            case 3 -> Calc.runGame();
-            case 4 -> GCD.runGame();
-            case 5 -> Progression.runGame();
-            case 6 -> Prime.runGame();
+            case GAME_CHOICE_EXIT -> scanner.close();
+            case GAME_CHOICE_GREET -> Cli.askUsername();
+            case GAME_CHOICE_EVEN -> Even.runGame();
+            case GAME_CHOICE_CALC -> Calc.runGame();
+            case GAME_CHOICE_GCD -> GCD.runGame();
+            case GAME_CHOICE_PROGRESSION -> Progression.runGame();
+            case GAME_CHOICE_PRIME -> Prime.runGame();
             default -> throw new RuntimeException("\"Invalid choice. Please try again.\" " + gameChoice);
         }
     }
